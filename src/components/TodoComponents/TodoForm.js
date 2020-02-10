7ce93c3a76ef7256d7ffb0dbfbb0fced47336099
kaverndsp <kaverndsp@users.noreply.github.com>
2020-02-10 15:28:11 -0600

    passing data to other components

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+
+class TodoForm extends Component {
+constructor(){
+    super()
+}
+
+render(){
+    return(
+        <form>
+            
+        </form>
+    )
+}
+
+}
+
+
+export default TodoForm;
